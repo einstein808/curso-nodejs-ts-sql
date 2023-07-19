1ce58796aaada2dad10cbdc5d674cb0da3e3406e
einstein808 <gabryel808einstein@gmail.com>
2023-07-19 14:28:06 -0300

    feat:validation tradução

--- src/server/controllers/cidades/Create.ts
+++ src/server/controllers/cidades/Create.ts
@@ -1,10 +1,34 @@
 import { Request, Response } from 'express';
-interface ICidade{
-    nome:string;
-}
+import { BAD_REQUEST, StatusCodes } from 'http-status-codes';
+import * as yup from 'yup';
+interface ICidade {
 
-export const create = (req: Request<{},{},ICidade>, res: Response) => {
-     
-    console.log(req.body.nome);
+    nome: string;
+    estado: string;
+}
+const bodyValidation: yup.Schema<ICidade> = yup.object().shape({
+    nome: yup.string().required().min(3)
+
+});
+export const create = async (req: Request<{}, {}, ICidade>, res: Response) => {
+    let validatedData: ICidade | undefined = undefined;
+    try {
+        validatedData = await bodyValidation.validate(req.body, { abortEarly: false });
+    } catch (error) {
+        const yupError = error as yup.ValidationError;
+        const validationError: Record<string, string> = {};
+        yupError.inner.forEach(error => {
+            if (error.path === undefined) return;
+            validationError[error.path] = error.message;
+
+        });
+        return res.status(StatusCodes.BAD_REQUEST).json({
+            errors: validationError,
+        });
+
+    }
+
+
+    console.log(validatedData);
     return res.send('Create');
 };
